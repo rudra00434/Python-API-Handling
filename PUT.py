0f4047b1217836903804d1165cc2dae4f22f32c3
Rudranil Goswami <nilrudra068@gmail.com>
2025-07-29 00:48:15 +0530

    Update PUT.py

--- PUT.py
+++ PUT.py
@@ -1,4 +1,4 @@
-✏️ PUT Request – Update existing data
+PUT Request – Update existing data
 payload = {"id": 1, "title": "updated"}
 response = requests.put("https://jsonplaceholder.typicode.com/posts/1", json=payload)
 print(response.json())
